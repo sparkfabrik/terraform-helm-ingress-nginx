1f0bc3ec9d5ad56088dd218e868580085110db50
Daniele Monti <daniele.monti@sparkfabrik.com>
2023-12-04 13:57:46 +0100

    refs platform/#2564: make namespace creation optional

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
 resource "kubernetes_namespace_v1" "this" {
+  count = var.create_namespace ? 1 : 0
+
   metadata {
-    labels = {
-      name = var.namespace
-    }
+    labels = merge(
+      {
+        name = var.namespace
+      },
+      var.namespace_additional_labels
+    )
     name = var.namespace
   }
+}
 
-  lifecycle {
-    ignore_changes = var.namespace_lifecycle_ignore_changes
+data "kubernetes_namespace_v1" "this" {
+  count = var.create_namespace ? 0 : 1
+
+  metadata {
+    name = var.namespace
   }
 }
 
@@ -16,7 +25,7 @@ resource "helm_release" "this" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
   version    = var.chart_version
-  namespace  = kubernetes_namespace_v1.ingress_nginx.metadata[0].name
+  namespace  = var.create_namespace ? kubernetes_namespace_v1.this[0].metadata[0].name : data.kubernetes_namespace_v1.this[0].metadata[0].name
 
   values = concat(
     [file("${path.module}/values.yaml")],
